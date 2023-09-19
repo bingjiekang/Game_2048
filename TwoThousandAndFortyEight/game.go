77573a22770f10cae3e831fbf6a913e07bf1eb73
kangbing <69076498+bingjiekang@users.noreply.github.com>
2023-09-19 23:46:27 +0800

    完成

--- TwoThousandAndFortyEight/game.go
+++ TwoThousandAndFortyEight/game.go
@@ -16,16 +16,47 @@ const (
 )
 
 type Game struct {
+	input      *Input
+	board      *Board
+	boardImage *ebiten.Image
+}
+
+// 创建一个新游戏对象
+func NewGame() (*Game, error) {
+	g := &Game{
+		input: NewInput(),
+	}
+	var err error
+	g.board, err = NewBoard(boardSize)
+	if err != nil {
+		return nil, err
+	}
+	return g, nil
 }
 
 // update控制更新当前帧率,每个tick都会被调用。tick是引擎更新的一个时间单位，默认为1/60s。tick的倒数我们一般称为帧，即游戏的更新频率。默认ebiten游戏是60帧，即每秒更新60次。
 func (g *Game) Update() error {
+	g.input.Update()
+	if err := g.board.Update(g.input); err != nil {
+		return err
+	}
 	return nil
 }
 
 // 每帧（frame）调用。帧是渲染使用的一个时间单位，依赖显示器的刷新率。如果显示器的刷新率为60Hz，Draw将会每秒被调用60次
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	if g.boardImage == nil {
+		g.boardImage = ebiten.NewImage(g.board.Size())
+	}
+	screen.Fill(BackgroundColor)
+	g.board.Draw(g.boardImage)
+	op := &ebiten.DrawImageOptions{}
+	sw, sh := screen.Bounds().Dx(), screen.Bounds().Dy()
+	bw, bh := g.boardImage.Bounds().Dx(), g.boardImage.Bounds().Dy()
+	x := (sw - bw) / 2
+	y := (sh - bh) / 2
+	op.GeoM.Translate(float64(x), float64(y))
+	screen.DrawImage(g.boardImage, op)
 }
 
 // 该方法接收游戏窗口的尺寸作为参数，返回游戏的逻辑屏幕大小。
